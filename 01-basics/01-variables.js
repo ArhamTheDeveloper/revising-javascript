f1d5455b6f9ffe56106b9d5185875ebd7360184b
ArhamTheDeveloper <arhamshaikh128@gmail.com>
2025-01-15 11:21:38 +0500

    Let const and var keywords understood

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,12 @@
+const userId = 985698;
+let userEmail = "user23@gmail.com";
+var userName = "Arham";
+userAge = 18;
+let accountState;
+
+console.table([userId, userEmail, userName, userAge, accountState]);
+
+/*
+Prefer not to use Var 
+Because of issue in Block Scope and Functional Scope
+*/
